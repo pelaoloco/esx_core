ee5c1f055d5e1b7cc7143a3a88901f1de23a0c9c
TheFantomas <117121911+TheFantomas@users.noreply.github.com>
2023-05-27 10:45:41 +0200

    formatting(esx_identity): format code

--- [core]/esx_identity/html/js/script.js
+++ [core]/esx_identity/html/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("message", (event) => {
   if (event.data.type === "enableui") {
-    document.body.classList[event.data.enable ? "remove" : "add"]("none");
+      document.body.classList[event.data.enable ? "remove" : "add"]("none");
   }
 });
 
@@ -12,27 +12,33 @@ document.querySelector("#register").addEventListener("submit", (event) => {
 
   const dateCheck = new Date(dofVal);
 
-  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(
-    dateCheck
+  const year = new Intl.DateTimeFormat("en", {
+      year: "numeric"
+  }).format(
+      dateCheck
   );
-  const month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(
-    dateCheck
+  const month = new Intl.DateTimeFormat("en", {
+      month: "2-digit"
+  }).format(
+      dateCheck
   );
-  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(
-    dateCheck
+  const day = new Intl.DateTimeFormat("en", {
+      day: "2-digit"
+  }).format(
+      dateCheck
   );
 
   const formattedDate = `${day}/${month}/${year}`;
   fetch("http://esx_identity/register", {
-    method: "POST",
-    body: JSON.stringify({
-      firstname: document.querySelector("#firstname").value,
-      lastname: document.querySelector("#lastname").value,
-      dateofbirth: formattedDate,
-      sex: document.querySelector("input[type='radio'][name='sex']:checked")
-        .value,
-      height: document.querySelector("#height").value,
-    }),
+      method: "POST",
+      body: JSON.stringify({
+          firstname: document.querySelector("#firstname").value,
+          lastname: document.querySelector("#lastname").value,
+          dateofbirth: formattedDate,
+          sex: document.querySelector("input[type='radio'][name='sex']:checked")
+              .value,
+          height: document.querySelector("#height").value,
+      }),
   });
 
   document.querySelector("#register").reset();
@@ -40,7 +46,7 @@ document.querySelector("#register").addEventListener("submit", (event) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   fetch("http://esx_identity/ready", {
-    method: "POST",
-    body: JSON.stringify({}),
+      method: "POST",
+      body: JSON.stringify({}),
   });
 });
